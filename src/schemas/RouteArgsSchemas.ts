1a709975855acc5620f754c84c3469530d165919
Grzegorz Beń <grzegorz.ben@coderush.pl>
2021-12-15 12:47:07 +0100

    moved default schema def into file

---
+++ src/schemas/RouteArgsSchemas.ts
@@ -0,0 +1,16 @@
+/**
+ * Default schema for common route parameters taken from url
+ */
+export const ROUTE_ARG_SCHEMA =
+{
+    Number: {
+        type: "number"
+    },
+    String: {
+        type: "string",
+        maxLength: 512
+    },
+    Boolean: {
+        type: "boolean"
+    }
+}
