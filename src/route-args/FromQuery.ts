4b669c1b690ac9b735dfa43720768b757a858b04
Grzegorz Beń <grzegorz.ben@coderush.pl>
2021-12-15 12:51:34 +0100

    query params compilation

--- src/route-args/FromQuery.ts
+++ src/route-args/FromQuery.ts
@@ -10,7 +10,21 @@ export class FromQuery extends RouteArgs {
     }
 
     public async extract(callData: IRouteCall, param: IRouteParameter, req: express.Request) {
-        return { CallData: callData, Args : req.query[param.Name] };
+        const arg = req.query[param.Name];
+        let result = null;
+
+        switch (param.RuntimeType.name) {
+
+            // query params are always sent as strings, even numbers,
+            // we must try to parse them as integers / booleans / objects
+            case "String": result = String(arg); break;
+            case "Number": result = Number(arg); break;
+            case "Boolean": result = ((arg as string).toLowerCase() === "true") ? true : false; break;
+            case "Object": result = JSON.parse(arg as string); break;
+            default: result = new param.RuntimeType(JSON.parse(arg as string)); break;
+        }
+
+        return { CallData: callData, Args: result};
     }
 
 }
